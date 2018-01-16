77ebdd5b87b6b0552e67fd81857b5a874d05061f
Sambulo Senda <sambulosendas@gmail.com>
2018-01-16 12:08:34 +0000

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render(){
+        return (
+<header>
+    <h2>Recipe Application</h2>
+    <nav>
+        <li>New Recipe</li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+    </nav>
+</header>
+
+
+        );
+    }
+
+
+}
+
+export default Navbar;
--- src/Recipeapplication.js
+++ src/Recipeapplication.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Recipeapplication.css';
+import Navbar from './Navbar';
 import Recipe from './Recipe';
 
 class Recipeapplication extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar/>
         <Recipe 
         title="Pastor" 
         ingredients= {['flour', 'water']}
